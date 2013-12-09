e8dae987cd982a00a51763926e91e7b710861b67
Matt Pfefferle <mpfefferle@juniper.net>
2013-12-09 09:36:13 -0500

    org mode initial settings

--- my-org-mode.el
+++ my-org-mode.el
@@ -10,7 +10,7 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 
 ;;; Folders and files
-(setq org-agenda-files (quote ("~/org/common")))
+(setq org-agenda-files (quote ("~/org/general" "~/org")))
 (setq org-directory "~/org")
 (setq org-default-notes-file "~/org/refile.org")
 
@@ -99,3 +99,34 @@
               ("n" "note" entry (file "~/org/refile.org")
                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t))))
 
+; Targets include this file and any file contributing to the agenda - up to 9 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
+; Use full outline paths for refile targets - we file directly with IDO
+(setq org-refile-use-outline-path t)
+
+; Targets complete directly with IDO
+(setq org-outline-path-complete-in-steps nil)
+
+; Allow refile to create parent tasks with confirmation
+(setq org-refile-allow-creating-parent-nodes (quote confirm))
+
+; Use IDO for both buffer and file completion and ido-everywhere to t
+(setq org-completion-use-ido t)
+(setq ido-everywhere t)
+(setq ido-max-directory-size 100000)
+(ido-mode (quote both))
+; Use the current window when visiting files and buffers with ido
+(setq ido-default-file-method 'selected-window)
+(setq ido-default-buffer-method 'selected-window)
+; Use the current window for indirect buffer display
+(setq org-indirect-buffer-display 'current-window)
+
+;;;; Refile settings
+; Exclude DONE state tasks from refile targets
+(defun bh/verify-refile-target ()
+  "Exclude todo keywords with a done state from refile targets"
+  (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+
+(setq org-refile-target-verify-function 'bh/verify-refile-target)
