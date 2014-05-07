d05d84dbf89f60a41de2588e329fab99a6e29bc4
Matt Pfefferle <mpfefferle@juniper.net>
2014-05-07 09:13:53 -0400

    Emacs OS X customization

--- emacs.d/setup-osx.el
+++ emacs.d/setup-osx.el
@@ -1,5 +1,9 @@
 (when (eq system-type 'darwin)
-  (setq mac-option-modifier 'alt)
+  (setq mac-option-modifier 'meta)
   (setq mac-command-modifier 'meta)
-  (global-set-key [kp-delete] 'delete-char))
+  (global-set-key [kp-delete] 'delete-char)
+
+  (setq exec-path (append exec-path '("/usr/local/bin")))
+
+  )
 (provide 'setup-osx)
